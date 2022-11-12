4156980c170c1259c093c136b1a44a86ca1f44fa
Aloysius Teo <aloysiusteo72@gmail.com>
2022-11-12 00:59:46 +0000

    Update 8 files

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  // target: "serverless",
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
+    //domains: ["storageapi.fleek.co", "savivets.wpengine.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.fleek.co/**",
+        pathname: "/20626237-8360-4375-83b2-2294cdded30d-bucket/assets/img/**",
+      },
+    ],
     domains: ["storageapi.fleek.co"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
+
+module.exports = nextConfig;
